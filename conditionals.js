f893d83e71156cb3fc8dce314765c8f02b2a3837
benjamin lew <bennru33@gmail.com>
2016-08-22 18:55:02 -0700

    homework

---
+++ conditionals.js
@@ -0,0 +1,88 @@
+var a = [10, 12, 14, 16, 20];
+
+for(x = 0; x < a.length; x++){
+  console.log(a[x] * 10);
+}
+
+while(x < a.length){
+  x = 0;
+  x++;
+
+}console.log(a[x]);
+
+// var grade = "A";
+//
+// if (grade === "A"){
+//   console.log("You are doing great in school!");
+// }else if(grade === "B"){
+//   console.log("You are doing good in school");
+// }else if(grade ==="C"){
+//   console.log("You are doing ok in school.");
+// }else if(grade === "D"){
+//   console.log("You need to try harder in school.");
+// }else if(grade === "F"){
+//   console.log("YOU FAIL!");
+// }else {
+//   console.log("Unexpected Value")
+// }
+//
+// switch (grade.toLowerCase()) {
+//   case "a":
+//     console.log("GREAT JOB!");
+//     break;
+//   case "b":
+//     Console.log("Good Job.")
+//     break;
+//   case "c":
+//     console.log("Doing alright");
+//     break;
+//   case "d":
+//     console.log("Try Harder!");
+//     break;
+//   case "f":
+//     console.log("EPIC FAILURE!!")
+//     break;
+//   default:
+//     console.log("Unexpected Value")
+// }
+
+
+// var weather = "windy";
+//
+// if(weather === "sunny"){
+//   console.log("Grab your sunglasses!")
+// }else if (weather === "rainy" || weather === "snow") {
+//   console.log("Grab an umbrella!")
+// }else if(weather === "windy"){
+//   console.log("Grab a windbreaker!")
+// }else {
+//   console.log("Grab a jacket!")
+// }
+
+
+// var cost = 124.49;
+// var shipping = 8.23
+// if (cost > 100 || shipping < 5) {
+//     console.log("Your shipping is free!");
+// }else{
+//   console.log("Your shipping is $10");
+// }
+
+
+// var age = (prompt("how old are you?"));
+//
+// if(age < 16){
+//   console.log("Go to school.");
+// }if( age > 15){
+//   console.log("You can drive.");
+// }if( age > 17){
+//   console.log("You can vote!");
+// }if( age > 20){
+//   console.log("You can buy alcohol!")
+// }if(age > 24){
+//   console.log("You can rent a car.");
+// }if (age > 34){
+//   console.log("You can run for President.");
+// }if (age > 61) {
+//   console.log("You can now collect Social Security.");
+// }
